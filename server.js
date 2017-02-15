e3be4599e8a00c637d3b28304e585965063a87fa
Shabin Hashim <shabin_hashim01@infosys.com>
2017-02-15 12:13:46 +0530

    timestamp changes

--- server.js
+++ server.js
@@ -1,10 +1,41 @@
 const express = require('express');
+const isNumber = require('isnumber');
+const moment = require('moment');
 
 
 let app = express();
 
-app.get('/' , function(req,res){
-	res.end("Hello world")
+let obj = {};
+
+let unix = null;
+let natural = null;
+let mom = null;
+app.get('/timestamp/:ts' , function(req,res){
+	unix = null;
+	natural = null;
+	let ts = req.params.ts;
+
+	let isUnix = moment.unix(ts).isValid();
+	//convert to natural
+	if(isUnix){
+		unix = ts;
+		mom = moment.unix(ts);
+		natural = mom.format("MMMM D, YYYY");
+	}
+	else{
+		let isNatural = moment(ts, "MMMM D, YYYY").isValid();
+		if(isNatural){
+			natural = ts;
+			mom = moment(ts, "MMMM D, YYYY");
+			unix = mom.unix();
+		}
+	}
+
+	obj.unix = unix;
+	obj.natural = natural
+
+	res.send(obj);
+
 })
 
 app.listen(3000);
